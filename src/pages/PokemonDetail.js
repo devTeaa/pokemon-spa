dd885e3ea007505b975a7d98fcc5ab5e9fbe0afb
devteaa <devtea@protonmail.com>
2021-09-10 23:10:40 +0700

    dummy pokemon-detail

---
+++ src/pages/PokemonDetail.js
@@ -0,0 +1,12 @@
+import './PokemonList.scss'
+import { useEffect, useState } from "react"
+
+const PokemonList = () => {
+  return (
+    <section>
+      Hello Detail
+    </section>
+  )
+}
+
+export default PokemonList
