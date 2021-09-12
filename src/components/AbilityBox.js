e4a2e6d0da9c69d3c4542f0a963ea39edfdeeeb7
devteaa <devtea@protonmail.com>
2021-09-12 19:30:18 +0700

    created AblityBox

---
+++ src/components/AbilityBox.js
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled'
+import {
+  useQuery,
+  gql
+} from '@apollo/client'
+
+const AbilityDiv = styled.div`
+  border: 1px solid #303030;
+  background-color: #F0f0f0;
+  border-radius: 4px;
+  font-family: Consolas;
+  text-transform: capitalize;
+  display: flex;
+  flex-direction: column;
+
+  > span,
+  > p {
+    padding: 2px 4px;
+  }
+
+  > span {
+    border-bottom: 1px solid #303030;
+  }
+
+  > p {
+    margin: 0;
+    font-size: 0.8em;
+  }
+`
+
+const AbilityBox = (props) => {
+  const ABILITY_DESC = gql`
+    query GetAbility($ability: String!) {
+      ability(ability: $ability) {
+        response
+      }
+    }
+  `
+
+  const { loading, error, data } = useQuery(ABILITY_DESC, {
+    variables: {
+      ability: props.ability.name
+    }
+  })
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error: {JSON.stringify(error)}</div>
+
+  return (
+    <AbilityDiv>
+      <span>{props.ability.name}</span>
+      <p>{data.ability.response.effect_entries.find(item => item.language.name === 'en').short_effect}</p>
+    </AbilityDiv>
+  )
+}
+
+export default AbilityBox
