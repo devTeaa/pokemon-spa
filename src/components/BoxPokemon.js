ab87e077ace246feb415a2ab51d1a6b331f34c71
devteaa <devtea@protonmail.com>
2021-09-13 16:24:14 +0700

    updated MyPokemonList UI

--- src/components/BoxPokemon.js
+++ src/components/BoxPokemon.js
@@ -3,10 +3,46 @@ import {
   gql
 } from "@apollo/client";
 import styled from "@emotion/styled"
+import { Link } from "react-router-dom"
 
 const BoxPokemonDiv = styled.div`
   display: flex;
   align-items: center;
+  position: relative;
+
+  &:before {
+    content: '';
+    display: block;
+    position: absolute;
+    background-image: url('/pokeball-logo.svg');
+    top: 0;
+    left: 0;
+    opacity: 0.6;
+    width: 60px;
+    height: 60px;
+  }
+
+  > img {
+    position: relative;
+    max-width: 80px;
+  }
+
+  > div {
+    padding-left: 4px;
+
+    span {
+      font-family: Consolas;
+      color: #FDFDFD;
+    }
+
+    a {
+      font-size: 0.7rem;
+      background-color: #38B0B8;
+      text-decoration: none;
+      padding: 2px 4px 4px 4px;
+      color: #FDFDFD;
+    }
+  }
 `
 
 const BoxPokemon = (props) => {
@@ -34,7 +70,11 @@ const BoxPokemon = (props) => {
   return (
     <BoxPokemonDiv className="pokemon-box">
       <img alt="sprite" src={data.pokemon.sprites.front_default} />
-      <span>{props.pokemon.pokemonName}</span>
+      <div>
+        <span>{props.pokemon.pokemonName}</span>
+        <br />
+        <Link to={'/detail/' + props.pokemon.name}>Detail</Link>
+      </div>
     </BoxPokemonDiv>
   )
 }
