18caedff6e12b14c4f2e38633894ce3a82fa7bbd
devteaa <devtea@protonmail.com>
2021-09-13 12:54:47 +0700

    update CatchPokemon UI

--- src/components/CatchPokemon.js
+++ src/components/CatchPokemon.js
@@ -1,46 +1,90 @@
-import { useState } from "react"
+import styled from "@emotion/styled"
+import PokemonLogo from '../assets/pokeball-logo.svg'
 
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const PokeballButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+
+  > img {
+    width: 40px;
+  }
+`
 
 const CatchPokemon = (props) => {
-  const [isCaught, setIsCaught] = useState(false)
-  const [myPokemonName, setMyPokemonName] = useState('')
+  const MySwal = withReactContent(Swal)
 
   const catchPokemon = () => {
     if (Math.random() >= 0.5) {
-      setIsCaught(true)
-    }
-  }
-
-  const savePokemon = (pokemon) => {
-    const myPokemonList = JSON.parse(window.localStorage.getItem('myPokemonList') || '[]')
-
-    if (myPokemonList.map(item => item.pokemonName).indexOf(myPokemonName) !== -1) {
-      window.alert('Please choose a different name')
+      MySwal.fire({
+        icon: 'error',
+        title: 'Failed to catch',
+        timer: 1500,
+        showConfirmButton: false,
+      })
 
       return
     }
 
-    window.localStorage.removeItem('myPokemonList')
-    window.localStorage.setItem('myPokemonList', JSON.stringify([
-      ...myPokemonList,
-      {
-        pokemonName: myPokemonName,
-        id: pokemon.id,
-        name: pokemon.name,
-      },
-    ]))
+    MySwal.fire({
+      icon: 'success',
+      title: 'Pokemon has been caught',
+      timer: 1500,
+      showConfirmButton: false,
+    }).then(() => {
+      MySwal.fire({
+        title: 'Input pokemon name',
+        text: '(must be unique)',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        showLoaderOnConfirm: true,
+        preConfirm: (myPokemonName) => {
+          try {
+            const myPokemonList = JSON.parse(window.localStorage.getItem('myPokemonList') || '[]')
+
+            if (myPokemonList.map(item => item.pokemonName).indexOf(myPokemonName) !== -1) {
+              throw new Error('Please choose a different name')
+            }
+
+            window.localStorage.removeItem('myPokemonList')
+            window.localStorage.setItem('myPokemonList', JSON.stringify([
+              ...myPokemonList,
+              {
+                pokemonName: myPokemonName,
+                id: props.pokemon.id,
+                name: props.pokemon.name,
+              },
+            ]))
+          } catch {
+            MySwal.showValidationMessage(
+              'Pokemon name already exist'
+            )
+          }
+
+        },
+        allowOutsideClick: () => !MySwal.isLoading()
+      }).then(result => {
+        if (result.isConfirmed) {
+          MySwal.fire({
+            icon: 'success',
+            title: 'Pokemon has been saved',
+          })
+        }
+      })
+    })
   }
 
   return (
     <div>
-      {
-        !isCaught
-          ? <button type="button" onClick={() => catchPokemon()}>Catch</button>
-          : <div>
-            <input type="text" onChange={e => setMyPokemonName(e.target.value)} />
-            <button type="button" onClick={() => savePokemon(props.pokemon)}>Save</button>
-          </div>
-      }
+      <PokeballButton onClick={() => catchPokemon()}>
+        <img alt="pokeball" src={PokemonLogo} />
+      </PokeballButton>
     </div>
   )
 }
